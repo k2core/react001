6cb1122e58a80011dda4420e7bee4f88f2fc7dc7
k2core <7179062@gmail.com>
2023-04-09 23:02:22 +0900

    useState에서 함수 사용 방법(좋은 경우)
    렌더링 하는 순간을 파악해야한다.
    이것은 5씩 증가한다.
    set함수 안에서 함수로 구현해야 한다.
    
    Changes to be committed:
    modified:   src/components/Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -8,11 +8,11 @@ export default function Counter() {
       <button
         className="button"
         onClick={() => {
-          setCount(count + 1);
-          setCount(count + 1);
-          setCount(count + 1);
-          setCount(count + 1);
-          setCount(count + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
         }}
       >
         Add +
