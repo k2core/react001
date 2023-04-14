498cc5676b217308d9f2728cde772cdde67f5903
k2core <7179062@gmail.com>
2023-04-14 20:35:02 +0900

    useEffect 생애주기(완료)
    일단 useEffect를 사용해봤고,
    뒤에서는 좀더 제대로 사용하는 방법이 나온다.
    
    Changes to be committed:
    modified:   src/components/Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Products() {
   const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
 
-  // fetch("data/products.json")
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     console.log("뜨끈한 데이터를 네트워크에서 받아옴");
-  //     setProducts(data);
-  //     console.log(data);
-  //   });
+  useEffect(() => {
+    fetch("data/products.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("뜨끈한 데이터를 네트워크에서 받아옴");
+        setProducts(data);
+      });
+    return () => {
+      console.log("🧹 깨끗하게 청소하는 일들을 합니다.");
+    };
+  }, []);
 
   return (
     <>
