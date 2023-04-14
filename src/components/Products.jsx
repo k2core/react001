c369eeabd0504a6dde9e194bab44080903bd5e7d
k2core <7179062@gmail.com>
2023-04-14 20:09:13 +0900

    useEffect 생애주기(준비)
    렌더링 시점을 핸들링할 useEffect를 배운다.
    
    Changes to be committed:
    new file:   public/data/products.json
    new file:   public/data/sale_products.json
    new file:   src/AppProducts.jsx
    new file:   src/components/Products.jsx
    modified:   src/index.js

---
+++ src/components/Products.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function Products() {
+  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  return (
+    <>
+      <ul>
+        {products.map((product) => {
+          <li>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>;
+        })}
+      </ul>
+      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+    </>
+  );
+}
